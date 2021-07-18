bf3fb336cc76d1fb9126ceddf3aa9bac77c53bf9
Tanner-Wills <tannerwills1@gmail.com>
2021-07-18 18:05:19 -0400

    emergency meeting for red astronaut

--- src/RedAstronaut.java
+++ src/RedAstronaut.java
@@ -1,12 +1,19 @@
+import java.util.Arrays;
+
 public class RedAstronaut extends Player implements Impostor {
     //Instance Variable
     private String skill; //either inexperienced, experienced, or expert.
 
     //Constructor
+    public RedAstronaut(String name){
+        this(name, 15, "experienced");
+    }
+
     public RedAstronaut(String name, int susLevel, String skill){
         super(name, susLevel);
         this.skill = skill;
     }
+
 
 // ***** RedAstronaut Methods *****
     // Interface Methods
@@ -18,8 +25,38 @@ public class RedAstronaut extends Player implements Impostor {
     }
 
     // Super Class Methods
-    public void emergencyMeeting(){
-        return;
+    public void emergencyMeeting() {
+        if (this.isFrozen()) {
+            return;
+        }
+        //sort players by increasing order of susLevel
+        Arrays.sort(getPlayers());
+
+        //Identify player with highest susLevel (NOT frozen, NOT RedAstronaut)
+        //If two highest susLevels are equal, no one gets frozen.
+        Player[] suspects = new Player[2];
+        int index = 0;
+
+        for (int i = getPlayers().length - 1; i >= 0; i--) {
+            if (!getPlayers()[i].isFrozen() && getPlayers()[i].getName() != this.getName()) {
+                suspects[index] = getPlayers()[i];
+                index++;
+            }
+            if (index == 2) {
+                break;
+            }
+        }
+        if (suspects[0] != null) {
+            if (suspects[1] == null || suspects[0].getSusLevel() != suspects[1].getSusLevel()) {
+                suspects[0].setFrozen(true);
+            }
+        }
+    }
+
+
+
+
+
     }
 
 
